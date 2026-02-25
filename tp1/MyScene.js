795c8cfd9cdb0adc50393d14f47acdafde2b4c4e
Tomás Jardim <up202306202@edu.fe.up.pt>
2026-02-25 12:54:07 +0000

    TP1

--- tp1/MyScene.js
+++ tp1/MyScene.js
@@ -1,5 +1,10 @@
 import { CGFscene, CGFcamera, CGFaxis } from "../lib/CGF.js";
 import { MyDiamond } from "./MyDiamond.js";
+import { MyParallelogram } from "./MyParallelogram.js";
+import { MyTriangle } from "./MyTriangle.js";
+import { MyTriangleBig } from "./MyTriangleBig.js";
+import { MyTriangleSmall } from "./MyTriangleSmall.js";
+
 
 /**
  * MyScene
@@ -26,9 +31,19 @@ export class MyScene extends CGFscene {
     //Initialize scene objects
     this.axis = new CGFaxis(this);
     this.diamond = new MyDiamond(this);
+    this.triangle = new MyTriangle(this);
+    this.parallelogram = new MyParallelogram(this);
+    this.triangleSmall = new MyTriangleSmall(this);
+    this.triangleBig = new MyTriangleBig(this);
+
 
     //Objects connected to MyInterface
     this.displayAxis = true;
+    this.displayTriangle = true;
+    this.displayDiamond = true;
+    this.displayParallelogram=true;
+    this.displayTriangleSmall = true;
+    this.displayTriangleBig = true;
     this.scaleFactor = 1;
   }
   initLights() {
@@ -91,8 +106,18 @@ export class MyScene extends CGFscene {
 
     // ---- BEGIN Primitive drawing section
 
-    this.diamond.display();
+    if (this.displayTriangle) this.triangle.display();
+
+    if (this.displayDiamond) this.diamond.display();
+
+    if (this.displayParallelogram) this.parallelogram.display();
+
+    if (this.displayTriangleSmall) this.triangleSmall.display();
+
+
+    if (this.displayTriangleBig) this.triangleBig.display();
 
     // ---- END Primitive drawing section
+
   }
 }
