a90dc0675bcf119305a3d95a8b680a94f5d9edd2
Tiago Almeida <up202303450@edu.fe.up.pt>
2026-03-04 18:37:38 +0000

    tp2-2

--- tp2/MyScene.js
+++ tp2/MyScene.js
@@ -1,5 +1,6 @@
 import { CGFscene, CGFcamera, CGFaxis } from "../lib/CGF.js";
 import { MyTangram } from "./MyTangram.js";
+import { MyUnitCube } from "./MyUnitCube.js";
 
 /**
  * MyScene
@@ -26,10 +27,12 @@ export class MyScene extends CGFscene {
     //Initialize scene objects
     this.axis = new CGFaxis(this);
     this.tangram = new MyTangram(this);
+    this.cube = new MyUnitCube(this);
     //Objects connected to MyInterface
     this.displayAxis = true;
     this.scaleFactor = 1;
     this.displayTangram = true;
+    this.displayCube = true;
   }
   initLights() {
     this.lights[0].setPosition(15, 2, 5, 1);
@@ -91,8 +94,12 @@ export class MyScene extends CGFscene {
 
     // ---- BEGIN Primitive drawing section
 
+    this.pushMatrix();
+    this.rotate(-Math.PI / 2, 1, 0, 0);
+    this.translate(1, -1, 1);
     if (this.displayTangram) this.tangram.display();
-
+    if (this.displayCube) this.cube.display();
+    this.popMatrix();
     // ---- END Primitive drawing section
   }
 }
---
+++ tp2/MyUnitCube.js
@@ -0,0 +1,48 @@
+import {CGFobject} from '../lib/CGF.js';
+/**
+ * MyUnitCube
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+export class MyUnitCube extends CGFobject {
+    constructor(scene) {
+        super(scene);
+        this.initBuffers();
+    }
+    
+    initBuffers() {
+        this.vertices = [
+            -1, 1, -1,	//0
+            -1, -1, -1,	//1
+            1, 1, -1,	//2
+            1, -1, -1,	//3
+            -1, 1, 0,	//4
+            -1, -1, 0,	//5
+            1, 1, 0,	//6
+            1, -1, 0		//7
+        ];
+
+        //Counter-clockwise reference of vertices
+        this.indices = [
+            0, 1, 2,
+            1, 3, 2,
+            4, 6, 5,
+            5, 6, 7,
+            6, 2, 7,
+            3, 7, 2,
+            4, 0, 6,
+            2, 6, 0,
+            5, 1, 4,
+            0, 4, 1,
+            7, 3, 5,
+            1, 5, 3
+        ];
+
+        //The defined indices (and corresponding vertices)
+        //will be read in groups of three to draw triangles
+        this.primitiveType = this.scene.gl.TRIANGLES;
+
+        this.initGLBuffers();
+    }
+}
+
