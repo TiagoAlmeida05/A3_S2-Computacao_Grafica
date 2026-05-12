4e4c16b622be3e4d036ec381469817ee1f8084c5
Tiago Almeida <up202303450@edu.fe.up.pt>
2026-05-12 23:51:45 +0100

    added sky

--- project/MyScene.js
+++ project/MyScene.js
@@ -2,7 +2,6 @@ import { CGFscene, CGFcamera, CGFaxis, CGFappearance } from "../lib/CGF.js";
 import { MyPlane } from "./MyPlane.js";
 import { MySphere } from "./MySphere.js";
 
-
 /**
  * MyScene
  * @constructor
@@ -17,9 +16,9 @@ export class MyScene extends CGFscene {
     this.initCameras();
     this.initLights();
 
-    //Background color
-    this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    this.enableTextures(true);
 
+    this.gl.clearColor(0.38, 0.72, 0.95, 1.0);
     this.gl.clearDepth(100.0);
     this.gl.enable(this.gl.DEPTH_TEST);
     this.gl.enable(this.gl.CULL_FACE);
@@ -27,16 +26,14 @@ export class MyScene extends CGFscene {
 
     this.scaleFactor = 1.0;
 
-    //Initialize scene objects
+    // Initialize scene objects
     this.axis = new CGFaxis(this);
     this.plane = new MyPlane(this, 200, 100);
-    this.sphere = new MySphere(this, 30, 50, 50);
-    this.pinkAppearance = new CGFappearance(this);
-    this.pinkAppearance.setAmbient(1.0, 0.55, 0.75, 1.0);
-    this.pinkAppearance.setDiffuse(1.0, 0.55, 0.75, 1.0);
-    this.pinkAppearance.setSpecular(1.0, 0.8, 0.9, 1.0);
-    this.pinkAppearance.setEmission(1.0, 0.45, 0.7, 1.0);
-    this.pinkAppearance.setShininess(10.0);
+    this.sky = new MySphere(this, 200, 100, 80, true, false, 1, 1); 
+    this.sunSphere = new MySphere(this, 12, 32, 32, false, false, 1, 1); 
+    
+    this.cloudRotation = 0;
+    this.setUpdatePeriod(50);
 
     this.greenAppearance = new CGFappearance(this);
     this.greenAppearance.setAmbient(0.25, 0.6, 0.25, 1.0);
@@ -44,78 +41,87 @@ export class MyScene extends CGFscene {
     this.greenAppearance.setSpecular(0.2, 0.35, 0.2, 1.0);
     this.greenAppearance.setEmission(0.0, 0.1, 0.0, 1.0);
     this.greenAppearance.setShininess(10.0);
-    //Objects connected to MyInterface
+
+    this.skyAppearance = new CGFappearance(this);
+    this.skyAppearance.setAmbient(1.0, 1.0, 1.0, 1.0);
+    this.skyAppearance.setDiffuse(1.0, 1.0, 1.0, 1.0);
+    this.skyAppearance.setSpecular(0.0, 0.0, 0.0, 1.0);
+    this.skyAppearance.setEmission(1.0, 1.0, 1.0, 1.0);
+    this.skyAppearance.setShininess(1.0);
+    this.skyAppearance.loadTexture('images/sky_panorama.jpg');
+    this.skyAppearance.setTextureWrap('REPEAT', 'CLAMP_TO_EDGE');
+
     this.displayAxis = true;
     this.displayPlane = true;
-    this.displaySphere = true;
-
   }
+
   initLights() {
-    this.lights[0].setPosition(15, 2, 5, 1);
-    this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
+    this.lights[0].setPosition(0, 1, -1, 0);
+    this.lights[0].setAmbient(0.2, 0.2, 0.2, 1.0);
+    this.lights[0].setDiffuse(1.0, 0.95, 0.8, 1.0);
+    this.lights[0].setSpecular(1.0, 0.95, 0.8, 1.0);
     this.lights[0].enable();
     this.lights[0].update();
   }
+
   initCameras() {
-    this.camera = new CGFcamera(
-      0.4,
-      0.1,
-      500,
-      vec3.fromValues(0, 1.7, 25),
-      vec3.fromValues(0, 1.0, 24)
-    );
+    this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(0, 1.7, 25), vec3.fromValues(0, 1.0, 24));
   }
+
   setDefaultAppearance() {
     this.setAmbient(0.2, 0.4, 0.8, 1.0);
     this.setDiffuse(0.2, 0.4, 0.8, 1.0);
     this.setSpecular(0.2, 0.4, 0.8, 1.0);
     this.setShininess(10.0);
   }
+
   display() {
-    // ---- BEGIN Background, camera and axis setup
-    // Clear image and depth buffer everytime we update the scene
     this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
     this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
-    // Initialize Model-View matrix as identity (no transformation
     this.updateProjectionMatrix();
     this.loadIdentity();
-    // Apply transformations corresponding to the camera position relative to the origin
     this.applyViewMatrix();
 
-    // Draw axis
+    // Sky dome
+    this.pushMatrix();
+    const skyMatrix = this.getMatrix();
+    skyMatrix[12] = 0; skyMatrix[13] = 0; skyMatrix[14] = 0;
+    this.setMatrix(skyMatrix);
+
+    this.gl.disable(this.gl.DEPTH_TEST);
+    this.gl.depthMask(false);
+    this.gl.disable(this.gl.CULL_FACE);
+
+    this.scale(1, -1, 1);
+    
+    this.skyAppearance.apply();
+    this.sky.display();
+    
+    this.gl.disable(this.gl.BLEND);
+    this.gl.enable(this.gl.DEPTH_TEST);
+    this.gl.depthMask(true);
+    this.gl.enable(this.gl.CULL_FACE); 
+    this.popMatrix();
+
     if (this.displayAxis) this.axis.display();
 
     this.setDefaultAppearance();
 
     var sca = [
-      this.scaleFactor,
-      0.0,
-      0.0,
-      0.0,
-      0.0,
-      this.scaleFactor,
-      0.0,
-      0.0,
-      0.0,
-      0.0,
-      this.scaleFactor,
-      0.0,
-      0.0,
-      0.0,
-      0.0,
-      1.0,
+      this.scaleFactor, 0.0, 0.0, 0.0,
+      0.0, this.scaleFactor, 0.0, 0.0,
+      0.0, 0.0, this.scaleFactor, 0.0,
+      0.0, 0.0, 0.0, 1.0,
     ];
-
     this.multMatrix(sca);
 
-    // ---- BEGIN Primitive drawing section
-
-    this.pinkAppearance.apply();
-    if (this.displaySphere) this.sphere.display();
-
+    // Draw Ground
     this.greenAppearance.apply();
     if (this.displayPlane) this.plane.display();
+  }
 
-    // ---- END Primitive drawing section
+  update(currTime) {
+    this.cloudRotation += 0.0008; // Good speed for drifting sprites
+    if (this.cloudRotation > 2 * Math.PI) this.cloudRotation -= 2 * Math.PI;
   }
 }
--- project/MySphere.js
+++ project/MySphere.js
@@ -7,12 +7,20 @@ export class MySphere extends CGFobject {
      * @param radius - Raio da esfera
      * @param slices - Número de divisões em torno do eixo Z (sectores)
      * @param stacks - Número de divisões entre os polos
+     * @param inside - Inverte as normais para renderizar de dentro para fora
+     * @param half - Gera apenas a metade superior da esfera (dome)
+     * @param texScaleS - Repetições horizontais do mapa de textura
+     * @param texScaleT - Repetições verticais do mapa de textura
      */
-    constructor(scene, radius, slices, stacks) {
+    constructor(scene, radius, slices, stacks, inside = false, half = false, texScaleS = 1, texScaleT = 1) {
         super(scene);
         this.radius = radius;
         this.slices = slices;
         this.stacks = stacks;
+        this.inside = inside;
+        this.half = half;
+        this.texScaleS = texScaleS;
+        this.texScaleT = texScaleT;
         this.initBuffers();
     }
     
@@ -27,32 +35,38 @@ export class MySphere extends CGFobject {
         let s, t;                                     
 
         let sectorStep = 2 * Math.PI / this.slices;
-        let stackStep = Math.PI / this.stacks;
+        let stackStep = (this.half ? Math.PI / 2 : Math.PI) / this.stacks;
         let sectorAngle, stackAngle;
 
         // --- Geração de Vértices, Normais e TexCoords ---
         for (let i = 0; i <= this.stacks; ++i) {
             stackAngle = Math.PI / 2 - i * stackStep; 
-            xy = this.radius * Math.cos(stackAngle);       
-            z = this.radius * Math.sin(stackAngle);        
+            
+            let xz = this.radius * Math.cos(stackAngle);
+            y = this.radius * Math.sin(stackAngle);
 
             for (let j = 0; j <= this.slices; ++j) {
                 sectorAngle = j * sectorStep;         
 
                 // Posição (x, y, z)
-                x = xy * Math.cos(sectorAngle);       
-                y = xy * Math.sin(sectorAngle);       
+                x = xz * Math.cos(sectorAngle);       
+                z = xz * Math.sin(sectorAngle);       
                 this.vertices.push(x, y, z);
 
                 // Normal (nx, ny, nz)
-                nx = -x * lengthInv;
-                ny = -y * lengthInv;
-                nz = -z * lengthInv;
-                this.normals.push(nx, ny, nz);
+                nx = x * lengthInv;
+                ny = y * lengthInv;
+                nz = z * lengthInv;
+                if (this.inside) {
+                    this.normals.push(-nx, -ny, -nz);
+                } else {
+                    this.normals.push(nx, ny, nz);
+                }
 
                 // Coordenadas de textura (s, t)
                 s = j / this.slices;
-                t = i / this.stacks;
+                s *= this.texScaleS;
+                t = (1 - (i / this.stacks)) * this.texScaleT;
                 this.texCoords.push(s, t);
             }
         }
@@ -61,6 +75,14 @@ export class MySphere extends CGFobject {
         // k1--k1+1
         // |  / |
         // k2--k2+1
+        const pushTriangle = (a, b, c) => {
+            if (this.inside) {
+                this.indices.push(a, c, b);
+            } else {
+                this.indices.push(a, b, c);
+            }
+        };
+
         for (let i = 0; i < this.stacks; ++i) {
             let k1 = i * (this.slices + 1);     // início da stack atual
             let k2 = k1 + this.slices + 1;      // início da próxima stack
@@ -68,11 +90,11 @@ export class MySphere extends CGFobject {
             for (let j = 0; j < this.slices; ++j, ++k1, ++k2) {
                 // 2 triângulos por setor (exceto nos polos, mas esta lógica simplificada funciona)
                 if (i !== 0) {
-                    this.indices.push(k1, k1 + 1, k2);
+                    pushTriangle(k1, k1 + 1, k2);
                 }
 
                 if (i !== (this.stacks - 1)) {
-                    this.indices.push(k1 + 1, k2 + 1, k2);
+                    pushTriangle(k1 + 1, k2 + 1, k2);
                 }
             }
         }
